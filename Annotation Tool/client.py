d5356de11b316cbaa4dc79233944321c054be45e
Melmars <z5237028@ad.unsw.edu.au>
2021-01-20 11:28:36 +0800

    Add files via upload

--- Annotation Tool/client.py
+++ Annotation Tool/client.py
@@ -15,6 +15,8 @@ from PyQt5.QtWidgets import *
 import math
 import cv2 as cv
 import numpy as np
+import warnings
+warnings.filterwarnings("ignore")
 # UI related
 sys.path.append('UI')
 from annotationUI import Ui_MainWindow
@@ -30,6 +32,7 @@ from undistortion import *
 from coco import Coco
 from config import *
 import utils
+from ScrollBar import ScrollBar
 # grpc related
 sys.path.append('GRPC')
 import openpose_pb2
@@ -77,7 +80,7 @@ class cocoWindow(QDialog,coco_Ui_Dialog):
                 for key in cat["keypoints"]:
                     kpKey = mapCOCO2KPs[key]
                     if kpKey in mainWin.params["keyPoints"][idx]:
-                        coor = utils.mult(mainWin.params["keyPoints"][idx][kpKey][::-1],frame.shape[:2])
+                        coor = utils.multi(mainWin.params["keyPoints"][idx][kpKey][::-1],frame.shape[:2])
                         keypoints.extend(coor)
                         keypoints.extend([2])
                     else:
@@ -118,6 +121,34 @@ class addWindow(QDialog, add_Ui_Dialog):
         mainWin.printMsg(f'Key Point "{type_}" is added to the list at ({self.coor[0]:.2f}, {self.coor[1]:.2f}).')
         self.close()
 
+def grpcRAFT(mainWin,prev,next_):
+    prev = cv.imencode('.jpg',mainWin.videoCap[prev])[1].tobytes()
+    next_ = cv.imencode('.jpg',mainWin.videoCap[next_])[1].tobytes()
+    try:
+        print('send request')
+        with grpc.insecure_channel(mainWin.IP['RAFT']) as channel:
+            stub = raft_pb2_grpc.raftDetectStub(channel)
+            response = stub.Upload(raft_pb2.Send(Type='frames',
+                                                    frameInfo=raft_pb2.frameRAFT(prev=prev,next_=next_,
+                                                    r=4-mainWin.params["rotDeg"]//90)))
+        if response.status:
+            flo = np.frombuffer(response.forw,dtype='float16')
+        else:
+            mainWin.printMsg('RAFT calculate failed. Consult with the server.')
+            return None
+    except grpc._channel._InactiveRpcError as e:
+        print(e)
+        mainWin.printMsg('Cannot connect to the Server. Verify the server\'s IP/Port!')
+        return None
+    return flo
+
+def moveNextKPs(mainWin, flo, idx, gap, H, W):
+    cur_kps = mainWin.params["keyPoints"][idx-gap]
+    next_kps = mainWin.params["keyPoints"][idx]
+    move = utils.calcMove(flo,cur_kps,H,W,mainWin.CT)
+    for key in cur_kps:
+        next_kps[key] = utils.add(cur_kps[key],move[key])
+
 ###### opticalFlowUI ######
 class optFlowWindow(QDialog, optFlow_Ui_Dialog):
     def __init__(self, mainWin, parent=None):
@@ -136,7 +167,6 @@ class optFlowWindow(QDialog, optFlow_Ui_Dialog):
         if end==self.cur:
             reply = QMessageBox.critical(self, "Error", "Can not have same start and end page number!!", QMessageBox.Ok, QMessageBox.Ok)
             return
-
         type_ = self.cb_type.currentText()
         self.close()
 
@@ -145,37 +175,25 @@ class optFlowWindow(QDialog, optFlow_Ui_Dialog):
         W,H,D = mainWin.videoCap[self.cur].shape
         _optFlow = mainWin.RAFT_FW if direct>0 else mainWin.RAFT_BW
 
-        for idx in range(self.cur, end, direct):
-            print(idx,end='\r')
+        regions = utils.merge(mainWin.ScrollBar.getHightLight())
+        offset = int(direct<0)
 
-            offset = int(direct<0)
-            if _optFlow[idx-offset].sum()==0:
-                print('send request')
-                prev = cv.imencode('.jpg',mainWin.videoCap[idx])[1].tobytes()
-                next_ = cv.imencode('.jpg',mainWin.videoCap[idx+direct])[1].tobytes()
-                try:
-                    with grpc.insecure_channel(mainWin.IP['RAFT']) as channel:
-                        stub = raft_pb2_grpc.raftDetectStub(channel)
-                        response = stub.Upload(raft_pb2.Send(Type='frames',
-                                                             frameInfo=raft_pb2.frameRAFT(prev=prev,next_=next_,
-                                                             r=4-mainWin.params["rotDeg"]//90)))
-                    if response.status:
-                        flo = np.frombuffer(response.forw,dtype='float16').reshape((H,W,2))
-                    else:
-                        mainWin.printMsg('RAFT calculate failed. Consult with the server.')
-                        return
-                except grpc._channel._InactiveRpcError as e:
-                    print(e)
-                    mainWin.printMsg('Cannot connect to the Server. Verify the server\'s IP/Port!')
+        for idx in range(self.cur+direct, end+direct, direct):
+            print('idx',idx,end='\r')
+
+            if idx in regions:
+                continue
+            gap = utils.getGap(idx, direct, regions)
+            if idx-direct in regions or _optFlow[idx-gap-offset].sum()==0:
+                prev=idx-gap
+                flo = grpcRAFT(mainWin, prev, idx)
+                if flo is None:
                     return
+                flo = flo.reshape((H,W,2))
             else:
-                flo = _optFlow[idx-offset]
-            cur_kps = mainWin.params["keyPoints"][idx]
-            next_kps = mainWin.params["keyPoints"][idx+direct]
-            move = utils.calcMove(flo,cur_kps,H,W,mainWin.CT)
-            for key in cur_kps:
-                next_kps[key] = utils.add(cur_kps[key],move[key])
-            
+                flo = _optFlow[idx-gap]
+            # print('use',idx-gap,'calc',idx)
+            moveNextKPs(mainWin, flo, idx, gap, H, W)
         mainWin.progressBar.setValue(mainWin.progressCount())
         mainWin.updateWinTitle(False)
 
@@ -239,7 +257,7 @@ class configWindow(QDialog,config_Ui_Dialog):
         self.OPR.clicked.connect(self.OPReset)
         self.RFS.clicked.connect(self.RFSet)
         self.RFR.clicked.connect(self.RFReset)
-        self.Video.clicked.connect(self.ChangeVideo)
+        self.Video.clicked.connect(self.ChangeVideoPath)
         self.Undist.clicked.connect(mainWin.loadDistortionParameter)
     
     def OPSet(self):
@@ -270,7 +288,7 @@ class configWindow(QDialog,config_Ui_Dialog):
         mainWin.IP['RAFT'] = mainWin.IP['default']['RAFT']
         utils.fillIP(mainWin.IP['RAFT'],self.rfIP)
 
-    def ChangeVideo(self):
+    def ChangeVideoPath(self):
         mainWin = self.mainWin
         dlg = QFileDialog()
         path,_ = dlg.getOpenFileName(self, 'Open file', './', "Video files (*.mpeg *.mov *.wmv *.rmvb *.flv *.mp4 *.avi)")
@@ -285,8 +303,10 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
     def __init__ (self, parent=None):
         #Init window
         super(MyMainWindow, self).__init__(parent)
+        self.ScrollBar = ScrollBar() # put this before setupUi because ScrollBar setup background and foreground color
         self.setupUi(self)
-        
+        self.IntensityView.setCentralItem(self.ScrollBar)
+
         self.params = dict() # to save in *.annot file
         #File related variables
         self.params["path"] = None
@@ -328,9 +348,10 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
         self.actionLoad_Video.triggered.connect(self.loadVideo)
         self.actionLoad_Images.triggered.connect(self.loadImages)
         self.actionLoad_Annot.triggered.connect(self.loadAnnot)
-        self.actionLoad_RAFT.triggered.connect(self.loadOptFlow)
         #------------------#
         self.actionLoad_Parameter.triggered.connect(self.loadDistortionParameter)
+        self.actionLoad_RAFT.triggered.connect(self.loadOptFlow)
+        self.actionLoadIntCSV.triggered.connect(self.loadIntCSN)
         #------------------#
         self.actionSave.triggered.connect(self.saveAnnotation)
         self.actionSave_as.triggered.connect(self.saveAsAnnotation)
@@ -359,14 +380,14 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
         self.pb_DeletePoint.clicked.connect(self.keyPointDelete)
         self.pb_LoadUndist.clicked.connect(self.loadDistortionParameter)
         self.pb_Openpose.clicked.connect(self.Openpose)
-        self.pb_CalcOptFlow.clicked.connect(self.calcOptFlow)
+        self.pb_CalcOptFlow.clicked.connect(self.getOptFlow)
         self.pb_Prop.clicked.connect(self.optFlow)
         ## Image Window ##
         self.cb_autoSave.stateChanged.connect(self.setAutoSave)
         self.pb_rotateC.clicked.connect(self.rotateClock)
         self.pb_rotateAC.clicked.connect(self.rotateAntiClock)
         ## Slider ##
-        self.horizontalScrollBar.valueChanged.connect(self.slide)
+        self.ScrollBar.valueChanged.connect(self.slide)
         self.sb_Page.valueChanged.connect(self.spinBox)
 
 ###### FILE I/O FUNCTION ######
@@ -486,9 +507,9 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
     def resetWrapper(self):
         temp = self.params["page"]
         self.RAFT_FW,self.RAFT_BW = None,None
-        self.horizontalScrollBar.setMaximum(self.maxpage-1)
         self.params["page"] = temp
-        self.horizontalScrollBar.setSliderPosition(self.params["page"])
+        self.ScrollBar.setMaximum(self.maxpage)
+        self.ScrollBar.setPos(self.params["page"]+1)
         self.progressBar.setMaximum(self.maxpage)
         self.sb_Page.setEnabled(True)
         self.sb_Page.setMinimum(1)
@@ -573,10 +594,10 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
                 self.keyPointList.setCurrentRow(self.keyPointList.currentRow()+1)
         elif event.key() == Qt.Key_Left:
             self.params["page"] = max(self.params["page"]-1,0)
-            self.horizontalScrollBar.setValue(self.params["page"])
+            self.ScrollBar.setPos(self.params["page"]+1)
         elif event.key() == Qt.Key_Right:
             self.params["page"] = min(self.params["page"]+1,self.maxpage-1)
-            self.horizontalScrollBar.setValue(self.params["page"])
+            self.ScrollBar.setPos(self.params["page"]+1)
         elif event.key() == Qt.Key_Tab:
             if self.keyPointList.currentRow() == -1:
                 self.keyPointList.setCurrentRow(0)
@@ -588,6 +609,8 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
             self.A = True
         elif event.key() == Qt.Key_P:
             self.optFlow()
+        elif str(event.key()) == '16777220': #Qt.Key_Enter
+            self.ScrollBar.addHighLight()
     
     #KeyRelease
     def keyReleaseEvent(self, event):
@@ -648,6 +671,19 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
         self.UndistAddressWin.setPlainText(self.params["undistPath"])
         self.printMsg(f'Undistortion Parameter file loaded successfully from {fname}')
 
+    #Load the pre-calculated Intensity Datas
+    def loadIntCSN(self):
+        dlg = QFileDialog()
+        fname,_ = dlg.getOpenFileName(self, 'Open Intensity Data File', './', "Intensity datas (*.csv)")
+        if not fname:
+            return
+        intensity = np.loadtxt(open(fname,'rb'),delimiter=',')
+        if len(intensity) != self.maxpage:
+            reply = QMessageBox.critical(self, "Error", "Incorrect CSV File, frames length not match!", QMessageBox.Ok, QMessageBox.Ok)
+            return 
+        self.ScrollBar.newPlot(intensity)
+        self.printMsg(f'Intensity Data File loaded successfully from {fname}')
+
     #Check if Undistortion enabled without *.yml file
     def checkUndEnable(self):
         if self.UndistEnable.checkState() and not self._undist:
@@ -704,13 +740,8 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
         self.update()
 
 ###### ALGORITHM ######
-    #Connection with Openpose
-    def Openpose(self):
-        if not self.videoCap:
-            reply = QMessageBox.critical(self, "Error", "Empty Image Collection!!", QMessageBox.Ok, QMessageBox.Ok)
-            return
-        
-        self.printMsg('Waiting for key points from Openpose Server...')
+    #open grpc tunnel with server for Openpose Keypoint predictions
+    def grpcOP(self):
         try:
             # self.connectionTimer.start(5000)
             cursor = self.params["page"]
@@ -724,36 +755,39 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
                                                          filename='%s' % cursor,
                                                          h=h,w=w,c=c,r=self.params["rotDeg"]))
             # self.connectionTimer.stop()
-            kp = eval(response.result.decode(encoding='utf-8'))
-            for key in kp:
-                self.params["keyPoints"][cursor][key] = kp[key]
-            self.updateWinTitle(False)
-            msg = self.params["keyPoints"][cursor]
-            print(f"Msg received from server: {msg}")
-            self.loadKeyPointList()
-            if kp:
-                self.printMsg('Key points received from Openpose Server!!')
-                self.progressBar.setValue(self.progressCount())
-            else:
-                self.printMsg('Received EMPTY key point from Openpose Server.')
+            kpDict = eval(response.result.decode(encoding='utf-8'))
+            return kpDict
         except grpc._channel._InactiveRpcError:
             self.printMsg('Cannot connect to the Server. Verify the server\'s IP/Port!')
+            return None
 
-    #Calculate Optical Flow using RAFT
-    def calcOptFlow(self):
+    #Get Openpose Keypoints from Server
+    def Openpose(self):
         if not self.videoCap:
             reply = QMessageBox.critical(self, "Error", "Empty Image Collection!!", QMessageBox.Ok, QMessageBox.Ok)
             return
+        cursor = self.params["page"]
+        kpDict = self.grpcOP()        
+        if kpDict:
+            for key in kpDict:
+                self.params["keyPoints"][cursor][key] = kpDict[key]
+            self.updateWinTitle(False)
+            self.loadKeyPointList()
+            self.printMsg('Key points received from Openpose Server!!')
+            self.progressBar.setValue(self.progressCount())
+        elif kpDict is not None:
+            self.printMsg('Received EMPTY key point from Openpose Server.')
 
-        assert isinstance(self.params["path"],str)
+    #open grpc tunnel with server for RAFT File
+    def grpcOptFlow(self, videoName):
+        assert isinstance(self.params["path"],str) # only support video for RAFT calculation
         MAX_MESSAGE_LENGTH = 1024*1024*1024
-        videoName = self.params["path"].split('/')[-1]
         try:
             dlg = QFileDialog()
             dlg.setFileMode(QFileDialog.Directory)
             path = dlg.getExistingDirectory(self, "Choose RAFT save path", "")
             if not path:
-                return False
+                return False,None
             with grpc.insecure_channel(self.IP['RAFT'],options=[
                 ('grpc.max_send_message_length', MAX_MESSAGE_LENGTH),
                 ('grpc.max_receive_message_length', MAX_MESSAGE_LENGTH),
@@ -761,27 +795,37 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
                 stub = raft_pb2_grpc.raftDetectStub(channel)
                 response = stub.Upload(raft_pb2.Send(Type='video',videoName=videoName))
                 if response.status:
-                    forw = response.forw
-                    backw = response.backw
-                    self.RAFT_FW = writeReadRAFT(path+'/forw.avi', forw)
-                    self.RAFT_BW = writeReadRAFT(path+'/backw.avi', backw)
-                    if utils.checkRAFT(self.videoCap, self.RAFT_FW, self.RAFT_BW):
-                        self.printMsg(f'Successfully fetch RAFT matrices from server!!')
-                        return True
-                    else:
-                        self.RAFT_FW = None
-                        self.RAFT_BW = None
-                        os.remove(path+'/forw.avi')
-                        os.remove(path+'/backw.avi')
-                        self.printMsg(f'Returned ERROR RAFT matrices from server!!')
-                        return False
+                    return response,path
                 else:
                     self.printMsg(f'Returned ERROR RAFT matrices from server!!')
-                    return False
+                    return False,None
         except grpc._channel._InactiveRpcError as e:
             print(e)
             self.printMsg('Cannot connect to the Server. Verify the server\'s IP/Port!')
-            return False
+            return False,None
+
+    #Get RAFT file from server
+    def getOptFlow(self):
+        if not self.videoCap:
+            reply = QMessageBox.critical(self, "Error", "Empty Image Collection!!", QMessageBox.Ok, QMessageBox.Ok)
+            return
+        videoName = (self.params["path"].split('/')[-1]).split('.')[0]
+        response,path = self.grpcOptFlow(videoName)
+        if response:
+            forw = response.forw
+            backw = response.backw
+            self.RAFT_FW = writeReadRAFT(path+'/'+videoName+'_forw.avi', forw)
+            self.RAFT_BW = writeReadRAFT(path+'/'+videoName+'_backw.avi', backw)
+            if utils.checkRAFT(self.videoCap, self.RAFT_FW, self.RAFT_BW):
+                self.printMsg(f'Successfully fetch RAFT matrices from server!!')
+                return True
+            else:
+                self.RAFT_FW = None
+                self.RAFT_BW = None
+                os.remove(path+'/'+videoName+'_forw.avi')
+                os.remove(path+'/'+videoName+'_backw.avi')
+                self.printMsg(f'Returned ERROR RAFT matrices from server!!')
+                return False
         
     #get RAFT matrix from server and move keypoints accordingly
     def optFlow(self):
@@ -807,13 +851,15 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
 
         kps = self.params["keyPoints"][self.params["page"]]
         if kps:
-            #按照从上到下从左到右的顺序显示在列表中
+            #show keypoints in ascending order of y and x
             kps = sorted(kps.items(),key=lambda item:(item[1][0],item[1][1]))
             self.keyPointList.addItems([f'{kp[0]}: ({kp[1][1]:.2f}, {kp[1][0]:.2f})' for kp in kps])
             self.keyPointList.setEnabled(True)
         else:
             self.keyPointList.addItem('Empty')
             self.keyPointList.setEnabled(False)
+        
+        #highlight selected key in KeyPoints
         for i in range(self.keyPointList.count()):
             if self.keyPointList.item(i).text().split(':')[0]==self.params["selectKey"]:
                 self.keyPointList.setCurrentRow(i)
@@ -901,7 +947,7 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
     
     #switch frame when slide
     def slide(self):
-        self.params["page"] = self.horizontalScrollBar.sliderPosition()
+        self.params["page"] = self.ScrollBar.Pos()-1
         self.sb_Page.setValue(self.params["page"]+1)
         self.loadKeyPointList()
         self.update()
@@ -909,8 +955,8 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
     
     #switch frame when edit pagebox
     def spinBox(self):
-        self.params["page"] = self.sb_Page.value()-1
-        self.horizontalScrollBar.setValue(self.params["page"]) #会触发 self.slide, 不需要做update. (spinBox基本上和slider绑定在一起)
+        self.params["page"] = self.sb_Page.value()-1 #会触发 self.slide, 不需要做update. (spinBox基本上和slider绑定在一起)
+        self.ScrollBar.setPos(self.params["page"]+1)
         self.sb_Page.setFocus()
 
 if __name__=="__main__":
